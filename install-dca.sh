04bd0dfaa2e7a09763a49736b2eb616e03baf798
AJ Rossman <aj.rossman@dominodatalab.com>
2023-08-30 10:40:46 -0700

    adding xgboost to install list

--- install-dca.sh
+++ install-dca.sh
@@ -8,6 +8,7 @@ source $KERNEL_NAME/bin/activate
 pip install dominodatalab-data
 pip install domino-code-assist==1.4.0rc1
 pip install ipykernel
+pip install xgboost
 
 python -m ipykernel install --user --name=$KERNEL_NAME
 
